64d5fc327d0cc5af53c95bf4a42f330832759a56
fabiante <fabiante@users.noreply.github.com>
2024-05-05 09:53:30 +0200

    Add option to serve openapi spec

--- sdk/setup.go
+++ sdk/setup.go
@@ -6,13 +6,16 @@ import (
 	"net/http"
 )
 
-type setupOption func(*setup) error
+type SetupOption func(*setup) error
 
 type setup struct {
+	handlers map[string]http.HandlerFunc
 }
 
-func Setup(opts ...setupOption) *setup {
-	s := &setup{}
+func Setup(opts ...SetupOption) *setup {
+	s := &setup{
+		handlers: make(map[string]http.HandlerFunc),
+	}
 
 	for _, opt := range opts {
 		if err := opt(s); err != nil {
@@ -30,6 +33,11 @@ func (s *setup) Run(handler func(ctx *Context) error) {
 
 	mux := http.NewServeMux()
 
+	// Register custom handlers
+	for path, handler := range s.handlers {
+		mux.HandleFunc(path, handler)
+	}
+
 	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		ctx := newContext(request, writer)
 		if err := handler(ctx); err != nil {
@@ -42,4 +50,18 @@ func (s *setup) Run(handler func(ctx *Context) error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func withHandler(path string, handler http.HandlerFunc) SetupOption {
+	return func(s *setup) error {
+		s.handlers[path] = handler
+		return nil
+	}
+}
+
+func WithOpenApiSpec(spec []byte) SetupOption {
+	return withHandler("/openapi.yml", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "application/yaml")
+		_, _ = writer.Write(spec)
+	})
 }
